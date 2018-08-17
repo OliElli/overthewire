24d6600e3ff8093b77eb068f574d4a74bba46328
Oliver Elliott <oliver.elliott@bristol.ac.uk>
2018-08-17 16:05:55 +0100

    updates

--- level1.py
+++ level1.py
@@ -19,10 +19,12 @@ output_bytes = output.hex()                     # Convert to hex
 pp.pprint(output_bytes)
 output_list = [output_bytes[i:i+8] for i in range(0,len(output_bytes), 8)]      
                                                 # Convert hex string into a list split on bytes
+print('output_list:')
+pp.pprint(output_list)
 for i in output_list[:4]:                       # Iterate first 4 bytes
     print(int(i, 16))                           # print the decimal value of the byte
-    # print(Decimal(int(i, 16)))
-    total = total + Decimal(int(i, 16))
+    # total = total + Decimal(int(i, 16))
+    total += Decimal(int(i, 16))
 
 print('Total: ' + str(total))
 output = str(total) + '\n'
