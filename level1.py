dc763729adff0d21f1d1b3a096d27e74acc64c7a
Oliver Elliott <oliver.elliott@gmail.com>
2018-08-23 22:23:34 +0100

    made it work (most of the time)

--- level1.py
+++ level1.py
@@ -1,34 +1,18 @@
 #!/usr/bin/env python
 
-from decimal import Decimal
-import io
-import pprint
+import struct
 import telnetlib
 import time
 
-pp = pprint.PrettyPrinter(indent=2)
-total = 0
-
-# Get input from telnet:
 print ('Connecting...')
 tn = telnetlib.Telnet('vortex.labs.overthewire.org', 5842)
-time.sleep(4)
 output = tn.read_some()                         # Grab the data
-pp.pprint(output)
-output_bytes = output.hex()                     # Convert to hex
-pp.pprint(output_bytes)
-output_list = [output_bytes[i:i+8] for i in range(0,len(output_bytes), 8)]      
-                                                # Convert hex string into a list split on bytes
-print('output_list:')
-pp.pprint(output_list)
-for i in output_list[:4]:                       # Iterate first 4 bytes
-    print(int(i, 16))                           # print the decimal value of the byte
-    # total = total + Decimal(int(i, 16))
-    total += Decimal(int(i, 16))
+         
+total = 0
+for i in [output[i:i+4] for i in range(0, len(output), 4)]:     # Split into bytes
+    total += struct.unpack_from('<I',i)[0]                      # Add integer value to total
+result = struct.pack('<I',(total & 0xffffffff))                 # Pack result into little endian bytes
 
-print('Total: ' + str(total))
-output = str(total) + '\n'
-
-tn.write(str(total).encode('ascii') + b'\n')
+tn.write(result)
 time.sleep(1)
 print (tn.read_some().decode('ascii'))
