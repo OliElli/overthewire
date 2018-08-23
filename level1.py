bbcdc4d74bb5635fd326ab0212dc150588e8fec8
Oliver Elliott <oliver.elliott@gmail.com>
2018-08-23 22:47:26 +0100

    4 spaces? what was I thinking

--- level1.py
+++ level1.py
@@ -6,11 +6,11 @@ import time
 
 print ('Connecting...')
 tn = telnetlib.Telnet('vortex.labs.overthewire.org', 5842)
-output = tn.read_some()                         # Grab the data
+output = tn.read_some()                                         # Grab the data
          
 total = 0
 for i in [output[i:i+4] for i in range(0, len(output), 4)]:     # Split into bytes
-    total += struct.unpack_from('<I',i)[0]                      # Add integer value to total
+  total += struct.unpack_from('<I',i)[0]                        # Add integer value to total
 result = struct.pack('<I',(total & 0xffffffff))                 # Pack result into little endian bytes
 
 tn.write(result)
